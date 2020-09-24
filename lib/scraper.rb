f5d49722260cd8f6c9257829a235323b3dfc0445
noella callahan <calla147@gmail.com>
2020-09-24 12:33:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,10 @@
-  
 require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
-# take the string of HTML returned by open-uri's open method 
-  # and convert it into a NodeSet (bunch of nested "nodes") 
-  # that we can easily play around with.
 doc = Nokogiri::HTML(html)
 
-# call .css on doc and give it the argument of our CSS selector:
 doc.css(".headline-26OIBN")
 
 # returned what looks like an Array containing one Nokogiri object
